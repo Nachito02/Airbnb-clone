701e5573abf3249571ea132b098e7b63a6747ac0
Juan Arguello <arguellojuan08@gmail.com>
2024-01-12 20:51:38 -0300

    add listings card

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import ClientOnly from "./components/ClientOnly";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -24,12 +25,15 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <NavBar currentUser={currentUser} />
-        <ToasterProvider />
-        <RegisterModal />
-        <LoginModal />
-        <RentModal />
-        {children}
+        <ClientOnly>
+          <NavBar currentUser={currentUser} />
+          <ToasterProvider />
+          <RegisterModal />
+          <LoginModal />
+          <RentModal />
+        </ClientOnly>
+
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
